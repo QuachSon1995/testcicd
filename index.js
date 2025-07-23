a069d82fd01269146d722791b9c2f541ca960126
QuachSon1995 <61349661+QuachSon1995@users.noreply.github.com>
2025-07-23 15:07:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/test', (req, res) => {
   res.send('This is a test endpoint1!');
 });
 
+app.get('/test1', (req, res) => {
+  res.send('This is a test endpoint1!');
+});
+
 // Khởi động server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
