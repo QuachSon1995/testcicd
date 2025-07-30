9b719d828d18bd10b04f6c592089e161a48856bd
QuachSon1995 <61349661+QuachSon1995@users.noreply.github.com>
2025-07-30 22:06:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.get('/test', (req, res) => {
   res.send('This is a test endpoint1!');
 });
 
-app.get('/test3', (req, res) => {
-  res.send('This is a test endpoint1!');
+app.get('/test4', (req, res) => {
+  res.send('This is a test endpoint4!');
 });
 
 // Khởi động server
